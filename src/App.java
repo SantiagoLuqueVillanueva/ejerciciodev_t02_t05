017836d9726a649ac17cbadfc8c622cb1e81d649
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-01 20:35:34 +0200

    tarea hecha

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int pesetas = 10000;
+        double conv = (int)(pesetas / 166.386);
+
+        System.err.printf("%d pesetas son %f euros", pesetas, conv);
     }
 }
